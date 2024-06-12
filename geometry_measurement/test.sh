cba56ac5503e8f25554b91249022eab5f16170ce
Violet Evergardern <88835096+saliteta@users.noreply.github.com>
2024-06-12 14:11:39 +0800

    Update test.sh

--- geometry_measurement/test.sh
+++ geometry_measurement/test.sh
@@ -9,18 +9,24 @@ MISSION_START_DATE=$(date +%Y%m%d)
 mkdir -p logs
 
 DATASETS=(
-    #/data/GauUscene/GauU_Scene/SZTU_COLMAP/
     /data/grocery_store/10F/
-    #/data/GauUscene/urbanscene3DrealScene/polytech
 )
 
-mask_folder=/home/xiongbutian/workspace/SA-GS/GroundedSAM_LITE/output/10F/npz
+mask_folders=(
+    ../GroundedSAM_LITE/output/10F/npz
+)
 
-# Get the number of available GPUs
-# GPU Counter to keep track of which GPU to use next
+prompts=(
+    ../prompt.txt
+)
+
+
 
 for i in "${!DATASETS[@]}"; do
     images_folder="${DATASETS[$i]}"
+    mask_folder="${mask_folders[$i]}"
+    prompt="${prompts[$i]}"
+
     if [ -d "$images_folder" ]; then # Check if it is a directory
         basename=$(basename "$images_folder")
         echo "This is the name of processed dataset: $basename"
@@ -30,7 +36,7 @@ for i in "${!DATASETS[@]}"; do
         
         
         # Run the process in the background
-        python main.py --image_path $images_folder/images --mask_path $mask_folder --prompts_file ../prompt.txt --output_file $output_file > $LOG_FILE_NAME 2>&1 &
+        python main.py --image_path $images_folder/images --mask_path $mask_folder --prompts_file $prompt --output_file $output_file > $LOG_FILE_NAME 2>&1 &
     fi
 done
 
